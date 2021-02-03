140959dfd8d29104205260f47dbf832db1c41e82
Satyam Seth <satyam1998.1998@gmail.com>
2021-02-03 19:30:02 +0530

    add Product model

--- shoppinglyx/app/models.py
+++ shoppinglyx/app/models.py
@@ -51,3 +51,22 @@ class Customer(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+CATEGORY_CHOICES=(
+    ('M','Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Wear'),
+    ('BW','Bottom Wear'),
+)
+
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discounted_price=models.FloatField()
+    description=models.TextField()
+    brand=models.CharField(max_length=100)
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    product_image=models.ImageField(upload_to='productimg')
+
+    def __str__(self):
+        return str(self.id)
