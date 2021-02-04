b7d09b9654076e23d81cba20a8519b2b6b02fbc6
Satyam Seth <satyam1998.1998@gmail.com>
2021-02-04 20:54:34 +0530

    make slider dynamic for home page

--- shoppinglyx/app/views.py
+++ shoppinglyx/app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Customer,Product,Cart,OrderPlaced
 
-def home(request):
- return render(request, 'app/home.html')
+class ProductView(View):
+    def get(self,request):
+        topwears=Product.objects.filter(category='TW')
+        bottomwears=Product.objects.filter(category='BW')
+        mobiles=Product.objects.filter(category='M')
+        return render(request,'app/home.html',{'topwears':topwears,'bottomwears':bottomwears,'mobiles':mobiles})
+# def home(request):
+#  return render(request, 'app/home.html')
 
 def product_detail(request):
  return render(request, 'app/productdetail.html')
