f3f15175128e0c12afe75d258e506ffd27e5c51d
Satyam Seth <satyam1998.1998@gmail.com>
2021-02-06 01:12:40 +0530

    implement profile page

--- shoppinglyx/app/views.py
+++ shoppinglyx/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Customer,Product,Cart,OrderPlaced
-from .forms import CustomerRegistrationForm
+from .forms import CustomerRegistrationForm,CustomerProfileForm
 from django.contrib import messages
 
 class ProductView(View):
@@ -21,9 +21,6 @@ def add_to_cart(request):
 
 def buy_now(request):
     return render(request, 'app/buynow.html')
-
-def profile(request):
-    return render(request, 'app/profile.html')
 
 def address(request):
     return render(request, 'app/address.html')
@@ -76,6 +73,24 @@ class CustomerRegistrationView(View):
             form.save()
         return render(request, 'app/customerregistration.html',{'form':form})
         
-
 def checkout(request):
     return render(request, 'app/checkout.html')
+
+class ProfileView(View):
+    def get(self,request):
+        form=CustomerProfileForm()
+        return render(request,'app/profile.html',{'form':form,'active':'btn-primary'})
+    
+    def post(self,request):
+        form=CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr=request.user
+            name=form.cleaned_data['name']
+            locality=form.cleaned_data['locality']
+            city=form.cleaned_data['city']
+            state=form.cleaned_data['state']
+            zipcode=form.cleaned_data['zipcode']
+            reg=Customer(user=usr,name=name,locality=locality,city=city,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request,'Congratulations!! Profile Updated Successfully')
+        return render(request,'app/profile.html',{'form':form,'active':'btn-primary'})
