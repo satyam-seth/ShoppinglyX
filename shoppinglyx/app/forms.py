7c9c0254fcaf2b8f18977b101f573cbf8b1867e4
Satyam Seth <satyam1998.1998@gmail.com>
2021-02-06 00:13:29 +0530

    implement password reset feature

--- shoppinglyx/app/forms.py
+++ shoppinglyx/app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField,PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField,PasswordChangeForm,PasswordResetForm,SetPasswordForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext,gettext_lazy as _
 from django.contrib.auth import password_validation
@@ -21,5 +21,12 @@ class LoginForm(AuthenticationForm):
 
 class MyPasswordChangeForm(PasswordChangeForm):
     old_password=forms.CharField(label=_('Old Password'),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
+    new_password1=forms.CharField(label='New Password',widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}),help_text=password_validation.password_validators_help_text_html())
+    new_password2=forms.CharField(label='Confirm New Password',widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}))
+
+class MyPasswordResetForm(PasswordResetForm):
+    email=forms.EmailField(label=_('Email'),max_length=254,widget=forms.EmailInput(attrs={'autocomplete':'email','class':'form-control'}))
+
+class MySetPasswordForm(SetPasswordForm):
     new_password1=forms.CharField(label='New Password',widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}),help_text=password_validation.password_validators_help_text_html())
     new_password2=forms.CharField(label='Confirm New Password',widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}))
