053e9ec176148ae53e64f5497d958a7085aa8c0f
Satyam Seth <satyam1998.1998@gmail.com>
2021-02-06 17:45:50 +0530

    implement orders page

--- shoppinglyx/app/views.py
+++ shoppinglyx/app/views.py
@@ -5,6 +5,8 @@ from .models import Customer,Product,Cart,OrderPlaced
 from .forms import CustomerRegistrationForm,CustomerProfileForm
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class ProductView(View):
     def get(self,request):
@@ -17,7 +19,7 @@ class ProductDeatilView(View):
     def get(self,request,pk):
         product=Product.objects.get(pk=pk)
         return render(request,'app/productdetail.html',{'product':product})
-
+@login_required
 def add_to_cart(request):
     user=request.user
     product_id=request.GET.get('prod_id')
@@ -25,6 +27,7 @@ def add_to_cart(request):
     Cart(user=user,product=product).save()
     return redirect('/cart')
 
+@login_required
 def show_cart(request):
     if request.user.is_authenticated:
         user=request.user
@@ -99,12 +102,15 @@ def remove_cart(request):
 def buy_now(request):
     return render(request, 'app/buynow.html')
 
+@login_required
 def address(request):
     add=Customer.objects.filter(user=request.user)
     return render(request, 'app/address.html',{'add':add,'active':'btn-primary'})
 
+@login_required
 def orders(request):
-    return render(request, 'app/orders.html')
+    op=OrderPlaced.objects.filter(user=request.user)
+    return render(request, 'app/orders.html',{'order_placed':op})
 
 def mobile(request,data=None):
     if data==None:
@@ -151,6 +157,7 @@ class CustomerRegistrationView(View):
             form.save()
         return render(request, 'app/customerregistration.html',{'form':form})
         
+@login_required
 def checkout(request):
     user=request.user
     add=Customer.objects.filter(user=user)
@@ -166,6 +173,7 @@ def checkout(request):
         totalamount=amount+shipping_amount
     return render(request, 'app/checkout.html',{'add':add,'totalamount':totalamount,'cart_items':cart_items})
 
+@login_required
 def payment_done(request):
     user=request.user
     custid=request.GET.get('custid')
@@ -176,6 +184,7 @@ def payment_done(request):
         c.delete()
     return redirect('orders')
 
+@method_decorator(login_required,name='dispatch')
 class ProfileView(View):
     def get(self,request):
         form=CustomerProfileForm()
